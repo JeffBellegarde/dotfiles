98b7abd2e5752e3535f3909f8b257fc5ad469df6
Jeff Bellegarde <Bellegarde@Jeffs-MacBook-Pro-2.local>
2015-03-10 16:15:22 -0700

    Add surfraw to path.

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,6 @@ set PATH ~/bin $PATH
 #set PATH ~/.rbenv/shims $PATH
 set PATH ~/go_src/bin $PATH
 set PATH /Applications/Emacs.app/Contents/MacOS/bin/ $PATH
+set PATH /usr/local/Cellar/surfraw/2.2.9/lib/surfraw $PATH
 
 eval (direnv hook fish)
